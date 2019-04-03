404272acdf8ec99295dac5861e83b418f9222cd0
Jason Benfell <c3331301@uon.edu.au>
2019-04-03 00:12:02 +0000

    Prints chars, prints individual char, prints the integer value of said char.
    Therefore, we can alter the integer, and we can alter the char, and we can change some text into some other text.

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 
 
 int main (){
-    int c;
     FILE * printTest;
     printTest = fopen("text.txt", "r");
+    char c;
+    char cha = 64;
+    int a = cha;
     while(1) {
         c = fgetc(printTest);
         if(feof(printTest)) { 
@@ -14,6 +16,9 @@ int main (){
     }
 
     fclose(printTest);
+    printf("\n %d \n", a);
+    printf("%c", cha);
+
     printf("\nProgram terminated.");
     return 0;
 }
